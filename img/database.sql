3427b04a829a0724f4175afd498fc29852ffb859
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-20 14:17:45 +0100

    update: query

--- img/database.sql
+++ img/database.sql
@@ -1,41 +1,46 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT * 
-FROM `students` 
-WHERE YEAR(`date_of_birth`) = 1990;
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE YEAR(`date_of_birth`) = 1990
+ORDER BY `date_of_birth`;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-SELECT * 
+SELECT `name`, `cfu`
 FROM `courses` 
 WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT *
-FROM `students`
-WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365 > 30;
+SELECT `name`, `surname`, `date_of_birth` 
+FROM `students` 
+WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365 > 30 
+ORDER BY `date_of_birth` DESC;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-SELECT * 
+SELECT `name`, `period`, `year`
 FROM `courses` 
-WHERE `period`= "I semestre" AND `year` = 1;
+WHERE `period`= "I semestre" AND `year` = 1
+ORDER BY `name`;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-SELECT * 
+SELECT `date`, `hour` 
 FROM `exams` 
 WHERE `date` = "2020-06-20" AND `hour` > "14:00";
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-SELECT * 
+SELECT `name` 
 FROM `degrees` 
-WHERE `level` = "magistrale";
+WHERE `level` = "magistrale"
+ORDER BY `name`;
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 SELECT COUNT(*) AS `num_departments` 
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT * 
+SELECT `name`, `surname`, `phone`
 FROM `teachers` 
-WHERE `phone` IS NULL;
+WHERE `phone` IS NULL
+ORDER BY `surname`;
 
 
 
