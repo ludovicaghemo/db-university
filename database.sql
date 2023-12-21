474d8c239ff44528cf071db8337abf49d9950f17
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-21 13:06:10 +0100

    fix: query

--- database.sql
+++ database.sql
@@ -8,23 +8,39 @@ ORDER BY `date_of_birth`;
 SELECT `name`, `cfu`
 FROM `courses` 
 WHERE `cfu` > 10;
+ORDER BY `cfu`;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT `name`, `surname`, `date_of_birth` 
-FROM `students` 
-WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365 > 30 
+-- SELECT `name`, `surname`, `date_of_birth` 
+-- FROM `students` 
+-- WHERE DATEDIFF(CURDATE(), `date_of_birth`) / 365 > 30 - non vengono però calcolati gli anni bisestili, quindi a meno che non si faccia /365.25 il risultato è approssimativo
 ORDER BY `date_of_birth` DESC;
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE date_of_birth <= CURDATE() - INTERVAL 30 YEAR
+ORDER BY `date_of_birth` DESC;
+-- Soluzione alternativa
+SELECT `name`, `surname`, `date_of_birth`, TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()) as `age`
+FROM `students`
+WHERE date_of_birth <= CURDATE() - INTERVAL 30 YEAR
+ORDER BY `age` DESC;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT `name`, `period`, `year`
 FROM `courses` 
-WHERE `period`= "I semestre" AND `year` = 1
+WHERE `period`= "I semestre"
+AND `year` = 1
 ORDER BY `name`;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT `date`, `hour` 
 FROM `exams` 
 WHERE `date` = "2020-06-20" AND `hour` > "14:00";
+-- Soluzione alternativa:
+SELECT `date`, `hour` 
+FROM `exams` 
+WHERE `date` = "2020-06-20" 
+AND HOUR(`hour`) >= 14;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT `name` 
@@ -37,7 +53,7 @@ SELECT COUNT(*) AS `num_departments`
 FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT `name`, `surname`, `phone`
+SELECT COUNT(*) AS `num_teachers` 
 FROM `teachers` 
 WHERE `phone` IS NULL
 ORDER BY `surname`;
