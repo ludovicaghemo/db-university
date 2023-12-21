7509531f1fe2be1543ad2944bcdb7e476c3f9c17
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-21 15:28:16 +0100

    add: group by queries

--- third/group_by.sql
+++ third/group_by.sql
@@ -4,5 +4,17 @@ FROM `students`
 GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`, COUNT(*) as `teachers_number`
+FROM `teachers`
+GROUP BY `office_address`
+ORDER BY `teachers_number`;
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`, AVG(`vote`)
+FROM `exam_student`
+GROUP BY `exam_id`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(*) as `courses_num`
+FROM `degrees`
+GROUP BY `department_id`;
