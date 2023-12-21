0bd55fb3dd1e8575810e6f05f0ab4af08185346c
ludovicaghemo <ludovica.aghemo@gmail.com>
2023-12-21 14:50:02 +0100

    add: query 1 - group by

--- third/group_by.sql
+++ third/group_by.sql
@@ -1,4 +1,8 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*) as `num_students`, YEAR(`enrolment_date`) as `enrl_date`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
